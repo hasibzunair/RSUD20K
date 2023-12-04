ee111435ab169e546bd6c9c15a2705188fe96941
Shakib-IO <khansakib299@gmail.com>
2023-12-03 20:40:53 -0500

    update yolov8s readme

--- object_detectors/yolov8s/test.py
+++ object_detectors/yolov8s/test.py
@@ -23,12 +23,6 @@ if __name__ == "__main__":
         help="Path to the dataset YAML file.",
     )
     parser.add_argument(
-        "--test_image_path",
-        default="../datasets/bdss20k/images/test/",
-        type=str,
-        help="Path to test data images.",
-    )
-    parser.add_argument(
         "--img_size", default=640, type=int, help="Image size (pixels) for inference."
     )
     parser.add_argument(
@@ -39,12 +33,6 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--device", default=0, type=str, help='CUDA device(s) to use or "cpu" for CPU.'
-    )
-    parser.add_argument(
-        "--save_dir",
-        default="predictions/",
-        type=str,
-        help="Directory to save prediction images.",
     )
     args = parser.parse_args()
 
@@ -66,19 +54,3 @@ if __name__ == "__main__":
         print(f"mAP@50:95 per category: {metrics.box.maps}")
     except Exception as e:
         print(f"An error occurred during evaluation: {e}")
-
-    # Ensure the save directory exists
-    os.makedirs(args.save_dir, exist_ok=True)
-
-    # Predict and save images
-    for image_name in os.listdir(args.test_image_path):
-        image_path = os.path.join(args.test_image_path, image_name)
-        if image_name.lower().endswith((".png", ".jpg", ".jpeg")):
-            try:
-                results = model.predict(image_path)
-                result_image = Image.fromarray(results[0].plot()[:, :, ::-1])
-                save_path = os.path.join(args.save_dir, image_name)
-                result_image.save(save_path)
-                print(f"Saved prediction for {image_name} at {save_path}")
-            except Exception as e:
-                print(f"An error occurred while processing {image_name}: {e}")
